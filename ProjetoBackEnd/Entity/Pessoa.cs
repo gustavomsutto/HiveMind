bebb29265549a377b1788daddf64f871f15fb054
gustavomsutto <gustavomsdev@gmail.com>
2015-09-16 18:01:37 -0300

    Commit-0001
    Initial commit.

---
+++ ProjetoBackEnd/Entity/Pessoa.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoBackEnd.Entity
+{
+    public class Pessoa
+    {
+        //declarar propriedades
+        public string Nome { get; set; }
+
+        //construtores
+        public Pessoa(string nome)
+        {
+            Nome = nome;
+        }
+
+        public Pessoa() {}
+
+
+
+    }
+}
